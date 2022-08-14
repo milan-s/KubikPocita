8d264389c7de3fc7f54cb8586675758e15a0d5db
milan-s <milan@srba.family>
2022-08-14 17:29:43 +0200

    Creating colour controlled input with white default

--- io.h
+++ io.h
@@ -7,6 +7,8 @@ void writeAnswer(int resultNumber);
 
 std::string getConfirmation(int drillsPassed, int drillsRequired);
 
+std::string getInput(int inputColor);
+
 std::tuple<int, std::string, std::string, std::string, std::string>
 changeTestParams(int drillCounts, std::string doAdd, std::string doSub, std::string doMul, std::string doDiv);
 
